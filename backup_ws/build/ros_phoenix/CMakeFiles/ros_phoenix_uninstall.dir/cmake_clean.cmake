e5c3dd9841bfdaf3749f5997a5750de1e6c4cd0e
drexellunabotic <drexellunabotics@gmail.com>
2024-10-30 19:58:47 -0400

    Add files via upload

---
+++ backup_ws/build/ros_phoenix/CMakeFiles/ros_phoenix_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ros_phoenix_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ros_phoenix_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
