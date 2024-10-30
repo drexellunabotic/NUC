ccc04eb3f73ed1ebd715cc6eaf1961879c93230b
drexellunabotic <drexellunabotics@gmail.com>
2024-10-30 19:54:04 -0400

    Add files via upload

---
+++ backup_ws/build/ros_phoenix/CMakeFiles/CTRE_PhoenixCCI-unzip.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CTRE_PhoenixCCI-unzip"
+  "ctre/5.21.2/cci/linux/x86-64/shared/libCTRE_PhoenixCCI.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/CTRE_PhoenixCCI-unzip.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ backup_ws/build/ros_phoenix/CMakeFiles/CTRE_PhoenixCCI-unzip.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for CTRE_PhoenixCCI-unzip.
+# This may be replaced when dependencies are built.
