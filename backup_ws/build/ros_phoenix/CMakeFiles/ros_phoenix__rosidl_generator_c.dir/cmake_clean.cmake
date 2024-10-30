da06dd5468299864ca74a08962e8ed04ab0a8426
drexellunabotic <drexellunabotics@gmail.com>
2024-10-30 19:56:09 -0400

    Add files via upload

---
+++ backup_ws/build/ros_phoenix/CMakeFiles/ros_phoenix__rosidl_generator_c.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ros_phoenix__rosidl_generator_c.dir/rosidl_generator_c/ros_phoenix/msg/detail/motor_control__functions.c.o"
+  "CMakeFiles/ros_phoenix__rosidl_generator_c.dir/rosidl_generator_c/ros_phoenix/msg/detail/motor_control__functions.c.o.d"
+  "CMakeFiles/ros_phoenix__rosidl_generator_c.dir/rosidl_generator_c/ros_phoenix/msg/detail/motor_status__functions.c.o"
+  "CMakeFiles/ros_phoenix__rosidl_generator_c.dir/rosidl_generator_c/ros_phoenix/msg/detail/motor_status__functions.c.o.d"
+  "libros_phoenix__rosidl_generator_c.pdb"
+  "libros_phoenix__rosidl_generator_c.so"
+  "rosidl_generator_c/ros_phoenix/msg/detail/motor_control__functions.c"
+  "rosidl_generator_c/ros_phoenix/msg/detail/motor_control__functions.h"
+  "rosidl_generator_c/ros_phoenix/msg/detail/motor_control__struct.h"
+  "rosidl_generator_c/ros_phoenix/msg/detail/motor_control__type_support.h"
+  "rosidl_generator_c/ros_phoenix/msg/detail/motor_status__functions.c"
+  "rosidl_generator_c/ros_phoenix/msg/detail/motor_status__functions.h"
+  "rosidl_generator_c/ros_phoenix/msg/detail/motor_status__struct.h"
+  "rosidl_generator_c/ros_phoenix/msg/detail/motor_status__type_support.h"
+  "rosidl_generator_c/ros_phoenix/msg/motor_control.h"
+  "rosidl_generator_c/ros_phoenix/msg/motor_status.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ros_phoenix__rosidl_generator_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
