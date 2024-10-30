5838b48753a15f608fd89588cd097530a1eff7a3
drexellunabotic <drexellunabotics@gmail.com>
2024-10-30 19:56:39 -0400

    Add files via upload

---
+++ backup_ws/build/ros_phoenix/CMakeFiles/ros_phoenix__rosidl_typesupport_c.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ros_phoenix__rosidl_typesupport_c.dir/rosidl_typesupport_c/ros_phoenix/msg/motor_control__type_support.cpp.o"
+  "CMakeFiles/ros_phoenix__rosidl_typesupport_c.dir/rosidl_typesupport_c/ros_phoenix/msg/motor_control__type_support.cpp.o.d"
+  "CMakeFiles/ros_phoenix__rosidl_typesupport_c.dir/rosidl_typesupport_c/ros_phoenix/msg/motor_status__type_support.cpp.o"
+  "CMakeFiles/ros_phoenix__rosidl_typesupport_c.dir/rosidl_typesupport_c/ros_phoenix/msg/motor_status__type_support.cpp.o.d"
+  "libros_phoenix__rosidl_typesupport_c.pdb"
+  "libros_phoenix__rosidl_typesupport_c.so"
+  "rosidl_typesupport_c/ros_phoenix/msg/motor_control__type_support.cpp"
+  "rosidl_typesupport_c/ros_phoenix/msg/motor_status__type_support.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ros_phoenix__rosidl_typesupport_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
