44c7f6e871f62b2ca98128b4eab0683d2c803ef8
drexellunabotic <drexellunabotics@gmail.com>
2024-10-30 19:56:24 -0400

    Add files via upload

---
+++ backup_ws/build/ros_phoenix/CMakeFiles/ros_phoenix__rosidl_generator_py.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ros_phoenix__rosidl_generator_py.dir/rosidl_generator_py/ros_phoenix/msg/_motor_control_s.c.o"
+  "CMakeFiles/ros_phoenix__rosidl_generator_py.dir/rosidl_generator_py/ros_phoenix/msg/_motor_control_s.c.o.d"
+  "CMakeFiles/ros_phoenix__rosidl_generator_py.dir/rosidl_generator_py/ros_phoenix/msg/_motor_status_s.c.o"
+  "CMakeFiles/ros_phoenix__rosidl_generator_py.dir/rosidl_generator_py/ros_phoenix/msg/_motor_status_s.c.o.d"
+  "rosidl_generator_py/ros_phoenix/libros_phoenix__rosidl_generator_py.pdb"
+  "rosidl_generator_py/ros_phoenix/libros_phoenix__rosidl_generator_py.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ros_phoenix__rosidl_generator_py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
