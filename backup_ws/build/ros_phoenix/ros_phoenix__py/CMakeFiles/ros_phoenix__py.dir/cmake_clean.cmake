06a2ba20c03d385836238bf196f1788d6d8e38f1
drexellunabotic <drexellunabotics@gmail.com>
2024-10-30 20:03:29 -0400

    Add files via upload

---
+++ backup_ws/build/ros_phoenix/ros_phoenix__py/CMakeFiles/ros_phoenix__py.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "../rosidl_generator_py/ros_phoenix/_ros_phoenix_s.ep.rosidl_typesupport_c.c"
+  "../rosidl_generator_py/ros_phoenix/_ros_phoenix_s.ep.rosidl_typesupport_fastrtps_c.c"
+  "../rosidl_generator_py/ros_phoenix/_ros_phoenix_s.ep.rosidl_typesupport_introspection_c.c"
+  "../rosidl_generator_py/ros_phoenix/msg/__init__.py"
+  "../rosidl_generator_py/ros_phoenix/msg/_motor_control.py"
+  "../rosidl_generator_py/ros_phoenix/msg/_motor_control_s.c"
+  "../rosidl_generator_py/ros_phoenix/msg/_motor_status.py"
+  "../rosidl_generator_py/ros_phoenix/msg/_motor_status_s.c"
+  "CMakeFiles/ros_phoenix__py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/ros_phoenix__py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ backup_ws/build/ros_phoenix/ros_phoenix__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/luna/backup_ws/build/ros_phoenix/ros_phoenix__py
+# Build directory: /home/luna/backup_ws/build/ros_phoenix/ros_phoenix__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
